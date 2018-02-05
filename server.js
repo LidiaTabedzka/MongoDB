2bcfdf18a8d3d0e5ef090c1cd66894935c39a20f
Lidia Tabędzka <lidia.tabedzka@gmail.com>
2018-02-05 15:48:49 +0100

    Add simple mongodb-app - a list of songs

--- server.js
+++ server.js
@@ -1,175 +1,72 @@
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const Schema = mongoose.Schema;
 const express = require('express');
 const app = express();
+
+app.use(express.static('./'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'pug');
+app.set('views','./views');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://Lidia:aga19gra@ds119258.mlab.com:19258/database-1');
 
 //new user Schema
-const userSchema = new Schema({
-    name: String,
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    admin: Boolean,
-    created_at: Date,
-    updated_at: Date
+const songsSchema = new Schema({
+    author: { type: String, required: true },
+    title: { type: String, required: true }
 });
 
-//Mongoose schema method
-userSchema.methods.manify = function(next) {
-    this.name = this.name + '-boy';
+const Song = mongoose.model('Song', songsSchema);
 
-    return next(null, this.name);
-};
-
-//pre-save method
-userSchema.pre('save', function(next) {
-    const currentDate = new Date();
-    this.updated_at = currentDate;
-
-    if (!this.created_at) {
-        this.created_at = currentDate;
-    }
-    next();
-});
-
-const User = mongoose.model('User', userSchema);
-
-const kenny = new User({
-    name: 'Kenny',
-    username: 'Kenny_the_boy',
-    password: 'password'
-});
-
-kenny.manify(function(err, name) {
-    if (err) throw err;
-    console.log('Twoje nowe imię to: ' + name);
-});
-
-kenny.save(function(err) {
-    if (err) throw err;
-    console.log('Uzytkownik ' + kenny.name + ' zapisany pomyslnie');
-});
-
-const benny = new User({
-    name: 'Benny',
-    username: 'Benny_the_boy',
-    password: 'password'
-});
-
-benny.manify(function(err, name) {
-    if (err) throw err;
-    console.log('Twoje nowe imię to: ' + name);
-});
-
-benny.save(function(err) {
-    if (err) throw err;
-    console.log('Uzytkownik ' + benny.name +  ' zapisany pomyslnie');
-});
-
-const mark = new User({
-    name: 'Mark',
-    username: 'Mark_the_boy',
-    password: 'password'
-});
-
-mark.manify(function(err, name) {
-    if (err) throw err;
-    console.log('Twoje nowe imię to: ' + name);
-});
-
-mark.save(function(err) {
-    if (err) throw err;
-    console.log('Uzytkownik ' + mark.name +  ' zapisany pomyslnie');
-});
-
-const findAllUsers = function() {
-    // find all users
-    return User.find({}, function(err, res) {
-        if (err) throw err;
-        console.log('Actual database records are ' + res);
-    });
-}
-
-const findSpecificRecord = function() {
-    // find specific record
-    return User.find({ username: 'Kenny_the_boy' }, function(err, res) {
-        if (err) throw err;
-        console.log('Record you are looking for is ' + res);
-    })
-}
-
-const updadeUserPassword = function() {
-    // update user password
-    return User.findOne({ username: 'Kenny_the_boy' })
-        .then(function(user) {
-            console.log('Old password is ' + user.password);
-            console.log('Name ' + user.name);
-            user.password = 'newPassword';
-            console.log('New password is ' + user.password);
-            return user.save(function(err) {
-                if (err) throw err;
-
-                console.log('Uzytkownik ' + user.name + ' zostal pomyslnie zaktualizowany');
-            })
-        })
-}
-
-const updateUsername = function() {
-    // update username
-    return User.findOneAndUpdate({ username: 'Benny_the_boy' }, { username: 'Benny_the_man' }, function(err, user) {
-        if (err) throw err;
-
-        console.log('Nazwa uzytkownika po aktualizacji to ' + user.username);
-    })
-}
-
-const findMarkAndDelete = function() {
-    // find specific user and delete
-    return User.findOne({ username: 'Mark_the_boy' })
-        .then(function(user) {
-            return user.remove(function() {
-                console.log('User successfully deleted');
-            });
-        })
-}
-
-const findKennyAndDelete = function() {
-    // find specific user and delete
-    return User.findOne({ username: 'Kenny_the_boy' })
-        .then(function(user) {
-            return user.remove(function() {
-                console.log('User successfully deleted');
-            });
-        });
-}
-
-const findBennyAndRemove = function() {
-    // find specific user and delete
-    return User.findOneAndRemove({ username: 'Benny_the_man' })
-        .then(function(user) {
-            return user.remove(function() {
-                console.log('User successfully deleted');
-            });
-        });
-}
-
-Promise.all([kenny.save(), mark.save(), benny.save()])
-    .then(findAllUsers)
-    .then(findSpecificRecord)
-    .then(updadeUserPassword)
-    .then(updateUsername)
-    .then(findMarkAndDelete)
-    .then(findKennyAndDelete)
-    .then(findBennyAndRemove)
-    .catch(console.log.bind(console));
-
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static('./'));
 app.get('/', function (req, res) {
-    res.sendFile('index.html');
+    res.render('index');
 });
+
+app.get('/add-songs', function (req, res) {
+    res.render('add-songs');
+});
+
+app.get('/songs-list', function (req, res) {
+    Song.find()
+        .then(function(song) {
+            res.render('songs-list', {data: song});
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+});
+
+app.get('/remove/:id', function (req, res) {
+    Song.findOneAndRemove({'_id': req.params.id})
+        .then(function(user) {
+            return user.remove(function() {
+                console.log('User successfully deleted');
+            });
+        })
+        .then(function() {
+            res.redirect('/songs-list');
+        })
+        .catch(function(err) {
+            console.log(err.message);
+        });
+});
+
+app.post('/addsong', function (req, res) {;
+    var newSong = new Song (req.body);
+    newSong.save()
+        .then(function() {
+            res.redirect('songs-list');
+        })
+        .catch(function(err) {
+            console.log(err.message);
+        });
+});
+
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
